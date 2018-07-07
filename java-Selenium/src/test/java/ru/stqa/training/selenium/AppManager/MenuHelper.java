34899187bbbd1413b77f5db32328d22e1cb068be
Konstantin <kruglovkn90@gmail.com>
2018-07-07 15:50:26 +0300

    домашнее задание№9

--- java-Selenium/src/test/java/ru/stqa/training/selenium/AppManager/MenuHelper.java
+++ java-Selenium/src/test/java/ru/stqa/training/selenium/AppManager/MenuHelper.java
@@ -42,32 +42,32 @@ public class MenuHelper extends HelperBase{
     public void countryCheck() {
         click(By.linkText("Countries"));
         List<String> countries = new ArrayList<String>();
-        List<WebElement> rows = driver.findElements(By.xpath(".//tr[@class = 'row']"));
+        List<WebElement> rows = driver.findElements(By.xpath("//tr[@class = 'row']"));
         for (int i=0; i<rows.size(); i++ ) {
-            WebElement cell = rows.get(i).findElement(By.xpath("//td[5]"));
+            WebElement cell = rows.get(i).findElement(By.xpath(".//td[5]"));
             WebElement country = cell.findElement(By.cssSelector("a"));
             String countryName = country.getText();
             countries.add(countryName);
         }
-        for (int i=0; i <countries.size(); i++) {
+        for (int i=0; i <countries.size()-1; i++) {
             Assert.assertTrue((countries.get(i).compareTo(countries.get(i+1)))<0);
         }
     }
 
     public void checkTimeZone() {
         click(By.linkText("Countries"));
-        List<WebElement> rows = driver.findElements(By.xpath(".//tr[@class = 'row']"));
+        List<WebElement> rows = driver.findElements(By.xpath("//tr[@class = 'row']"));
         for (int i = 0; i < rows.size(); i++) {
-            if (!rows.get(i).findElement(By.xpath("//td[6]")).getText().equals("0")) {
-                rows.get(i).findElement(By.xpath("//td[5]")).findElement(By.cssSelector("a")).click();
+            if (!(rows.get(i).findElement(By.xpath(".//td[6]")).getText()).equals("0")) {
+                rows.get(i).findElement(By.xpath(".//td[5]")).findElement(By.cssSelector("a")).click();
                 WebElement table = driver.findElement(By.cssSelector("#table-zones"));
                 List<String> timeZones = new ArrayList<String>();
                 List<WebElement> timeRows = table.findElements(By.cssSelector("tr"));
-                for (int a = 1; a < timeRows.size(); a++) {
-                    String timeZone = timeRows.get(a).findElement(By.xpath("//td[3]")).getText();
+                for (int a = 1; a < timeRows.size()-1; a++) {
+                    String timeZone = timeRows.get(a).findElement(By.xpath(".//td[3]")).getText();
                     timeZones.add(timeZone);
                 }
-                for (int t = 0; t < timeZones.size(); t++) {
+                for (int t = 0; t < timeZones.size()-1; t++) {
                     Assert.assertTrue((timeZones.get(t).compareTo(timeZones.get(t + 1))) < 0);
                 }
                 driver.navigate().back();
